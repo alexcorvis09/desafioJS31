4750851ca580cc9d7ca07bd7dbd1ec85fa751dc0
Ale Dominguez <aledomcru@hotmail.com>
2024-02-10 23:46:55 -0600

    adding posts

---
+++ js/postDom.js
@@ -0,0 +1,26 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+
+// let btnSubmit = document.querySelector("submitButton")
+// let postContent = document.querySelector("#postContent")
+// let postTitle = document.querySelector("postTitle")
+
+// btnSubmit.addEventListener("click", (e) => {
+//     e.preventDefault(),
+//     console.log ("Submitted")
+
+//     btnSubmitValidation()
+
+// })
+
+// let btnSubmitValidation = () => {
+//     if (input.value === "") {
+//       postContent.innerHTML = "No puede estar en blanco";
+//       console.log("failure")
+//     } 
+//       console.log("successs")
+//       postContent.innerHTML = ""
+//   };
+// })
+
+// ../index.html
